419e27552060650b22cb2e05bb64245e83275675
Johnny Ermio <johnnytheklaw@gmail.com>
2023-03-10 10:32:12 +0800

    fourth commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function index(){
 
-        $users = User::whereNotIn('name', ['admin'])->paginate(10);
+        $users = User::latest()->whereNotIn('name', ['admin'])->paginate(10);
 
         return view('admin.users.index', compact('users'));
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -214,8 +214,8 @@
                 <button @click="isOpen = !isOpen" class="p-1 bg-gray-200 rounded-full focus:outline-none focus:ring">
                   <img
                     class="object-cover w-8 h-8 rounded-full"
-                    src="https://avatars0.githubusercontent.com/u/57622665?s=460&u=8f581f4c4acd4c18c33a87b3e6476112325e8b38&v=4"
-                    alt="Ahmed Kamel"
+                    src="{{ asset('/images/profile.png') }}"
+                    alt="Profile"
                   />
                 </button>
                 <!-- green dot -->
